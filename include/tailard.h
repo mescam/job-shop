a6f4d2b8916c187d374a219e464edb7a8766f20d
Marcin Zaremba <mrcn.zrmb@gmail.com>
2013-10-21 15:31:24 +0200

    initial tailard files

---
+++ include/tailard.h
@@ -0,0 +1,6 @@
+#ifndef TAILARD_H
+#define TAILARD_H
+
+instance *tailard_loader(FILE *f, int n);
+
+#endif
---
+++ src/tailard.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "loader.h"
+#include "tailard.h"
+
+instance *tailard_loader(FILE *f, int n) {
+}
