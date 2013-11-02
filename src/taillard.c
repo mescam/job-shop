ecbe92095ad50b815ed829dce3d5567f09c48614
Marcin Zaremba <mrcn.zrmb@gmail.com>
2013-11-03 00:17:50 +0100

    with respect to n! fixes #1

--- src/taillard.c
+++ src/taillard.c
@@ -5,9 +5,13 @@
 #define TAILLARD_BUFF_MAX 256
 
 // read one matrix
-void taillard_read_lines(FILE *f, instance *pi) {
+void taillard_read_lines(FILE *f, instance *pi, int initial_n, int n) {
     char buff[TAILLARD_BUFF_MAX];
     int subject = 0;
+
+    // if n not specified, read all
+    if(n == 0)
+        n = pi->jobs;
 
     // read first line to determine what are we reading
     fscanf(f, "%s\n", buff);
@@ -16,12 +20,18 @@ void taillard_read_lines(FILE *f, instance *pi) {
         subject = 1;
 
     // iterate through whole matrix to fill detected parameter
-    for(int i = 0; i < pi->jobs; i++) {
+    for(int i = 0; i < n; i++) {
         for(int j = 0; j < pi->machines; j++) {
             fscanf(f, "%d ",
                     subject ? &(pi->listing[i][j].machine) : &(pi->listing[i][j].time));
         }
     }
+
+    // read this bunch of unused crap
+    int spare_reading = (initial_n - n) * pi->machines;
+
+    for(int i = 0; i < spare_reading; i++)
+        fscanf(f, "%*d ");
 }
 
 void taillard_fill_job_id(instance *pi) {
@@ -38,15 +48,20 @@ instance *taillard_loader(FILE *f, int n) {
 
     // grab first line essential info
     fscanf(f, "%d %d %*d %*d %*d %*d\n", &(pi->jobs), &(pi->machines));
+
+    // save initial number of jobs
+    int initial_jobs = pi->jobs;
+
+    printf("This is n: %d\n", n);
     if (n != 0 && pi->jobs > n)
         pi->jobs = n;
     alloc_listing(pi);
 
     // read times
-    taillard_read_lines(f, pi);
+    taillard_read_lines(f, pi, initial_jobs, n);
 
     // read machines
-    taillard_read_lines(f, pi);
+    taillard_read_lines(f, pi, initial_jobs, n);
 
     //fill job_id
     taillard_fill_job_id(pi);
