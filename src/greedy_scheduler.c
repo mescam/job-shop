012efac8be80c9c3f6d3fced0f77f4eaba633058
Jakub Woźniak <mescam@mescam.pl>
2013-11-03 23:35:47 +0100

    okay, we're done

--- src/greedy_scheduler.c
+++ src/greedy_scheduler.c
@@ -19,11 +19,9 @@ int greedy_feed_machines(instance *pi, int *iterators, task **machines,
     int i, j, min = INT_MAX;
     for(i = 0; i < pi->machines; i++) { //for every machine
         if(machines[i] == NULL) { //if machine is in idle state
-            //printf("Maszyna %d jest w stanie idle\n", i);
             for(j = 0; j < pi->jobs; j++) { //for every job
                 //if jobs needs our idle-machine
                 if(iterators[j] < pi->machines && pi->listing[j][iterators[j]].machine == i) {
-                    //printf("Praca dla maszyny %d to [%d][%d]\n", i, j, iterators[j]);
                     machines[i] = &(pi->listing[j][iterators[j]]);
                     result->schedule[j][iterators[j]] = T;
                     if(machines[i]->time < min)
@@ -39,17 +37,6 @@ int greedy_feed_machines(instance *pi, int *iterators, task **machines,
     return min;
 }
 
-/*int greedy_nearest_task_finish(instance *pi, task **machines) {
-    int i, min;
-    min = INT_MAX;
-    for(i = 0; i < pi->machines; i++) {
-        if(machines[i] != NULL && machines[i]->time < min)
-            min = machines[i]->time;
-    }
-    //printf("Najblizsza chwila zakonczenia to +%d\n", min);
-    return min;
-}*/
-
 void greedy_time_shift(task **machines, instance *pi, int timeshift, int *iterators) {
     int i;
     for(i = 0; i < pi->machines; i++) {
@@ -63,18 +50,6 @@ void greedy_time_shift(task **machines, instance *pi, int timeshift, int *iterat
     }
 }
 
-/*void greedy_check_done_tasks(task **machines, instance *pi, int *iterators) {
-    int i;
-    for(i = 0; i < pi->machines; i++) {
-        if (machines[i] != NULL && machines[i]->time == 0) {
-            //printf("Maszyna %d skonczyla prace, zwalniam zasob\n", i);
-            iterators[machines[i]->job_id]++;
-            //printf("Iterator dla pracy %d zwiekszony do %d\n",
-            //       machines[i]->job_id, iterators[machines[i]->job_id]);
-            machines[i] = NULL;
-        }
-    }
-}*/
 
 void greedy_scheduler(instance *pi, sched_result *result) {
     //first, we need to keep machines states
@@ -89,11 +64,8 @@ void greedy_scheduler(instance *pi, sched_result *result) {
     //initial machines states
     while(!greedy_are_we_ok(task_iterators, pi)) {
         int t = greedy_feed_machines(pi, task_iterators, machines, T, result);
-        //int t = greedy_nearest_task_finish(pi, machines);
         T += t;
-        //printf("Czas T=%lld\n", T);
         greedy_time_shift(machines, pi, t, task_iterators);
-        //greedy_check_done_tasks(machines, pi, task_iterators);
     }
     result->scheduling_time = T;
     //cleaning
